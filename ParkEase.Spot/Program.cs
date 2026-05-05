f29ca45e0a0f6311cafbb188c6e3fc76b98ece09
Loveesh Gulati <131939145+loveeshgulati@users.noreply.github.com>
2026-05-05 11:54:37 +0530

    chore: add Dockerfiles and render.yaml with full config

--- ParkEase.Spot/Program.cs
+++ ParkEase.Spot/Program.cs
@@ -63,7 +63,8 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host(builder.Configuration["RabbitMQ:Host"], "/", h =>
+        cfg.Host(builder.Configuration["RabbitMQ:Host"],
+                 builder.Configuration["RabbitMQ:VHost"] ?? "/", h =>
         {
             h.Username(builder.Configuration["RabbitMQ:Username"]!);
             h.Password(builder.Configuration["RabbitMQ:Password"]!);
